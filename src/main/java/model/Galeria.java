af41632d3eae31ff01669cd5f76d677998de7ae7
Rael <rael.cappra@gmail.com>
2015-10-02 15:32:39 -0300

    desquebrado removendo alguns @NotNull que nao eram pra existir

--- src/main/java/model/Galeria.java
+++ src/main/java/model/Galeria.java
@@ -15,8 +15,8 @@ import persistencia.ImagemDao;
  * @author Rael
  */
 public class Galeria {
-    private @NotNull long id;
-    private @NotNull Usuario usuario;
+    private long id;
+    private Usuario usuario;
     private @NotNull @Size(min=3, message="O nome da galeria deve ser maior que {min}") String nome;
 
     public long getId() {
